29f0e687ec68d5d39ecf17300ea55651abd888b4
mina <miina.nabeeh@gmail.com>
2019-08-20 17:07:00 +0200

    test9_tany_

--- TestCode.cpp
+++ TestCode.cpp
@@ -6,7 +6,7 @@ cout<<"start OF Project"<<endl;
 
 ofstream myFile;
 myFile.open("TestResult.txt");
-int x=5+0+10+1+11;
+int x=5+0+10+1+13;
 myFile<<"result is "<<x<<endl;
 cout<<"x is "<<x<<endl;
 cout<<"end Of Project"<<endl;
